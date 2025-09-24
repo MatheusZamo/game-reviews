4314f8b3fb5f80d6d47bb9e4a9f60aadae52ac86
MatheusZamo <19matheushenrique03@gmail.com>
2025-09-24 19:46:02 -0300

    Add Super Mario Bros. Wonder review

---
+++ app/reviews/super-mario-bros-wonder/page.jsx
@@ -0,0 +1,10 @@
+const Mario = () => {
+    return(
+        <>
+          <h1>Super Mario Bros. Wonder - Análise</h1>
+          <p>Análise de Super Mario Bros. Wonder aqui</p>
+        </>
+    )
+}
+
+export default Mario
