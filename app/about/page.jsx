a63c02d41468fabd6ba4c3811635730906ec5573
MatheusZamo <19matheushenrique03@gmail.com>
2025-09-24 20:26:31 -0300

    Add about page

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <>
+          <h1>Sobre o Análise de Jogos</h1>
+          <p>Descrição do Análise de Jogos aqui</p>
+        </>
+    )
+}
+
+export default About
