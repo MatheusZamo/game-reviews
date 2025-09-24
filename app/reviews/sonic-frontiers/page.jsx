1de05186fa57ced671182066aed768bfd0a28340
MatheusZamo <19matheushenrique03@gmail.com>
2025-09-24 20:33:46 -0300

    Add Sonic Frontiers Reviews

---
+++ app/reviews/sonic-frontiers/page.jsx
@@ -0,0 +1,10 @@
+const Sonic = () => {
+    return(
+        <>
+          <h1>Sonic Frontiers - Análise</h1>
+          <p>Análise de Sonic Frontiers aqui</p>
+        </>
+    )
+}
+
+export default Sonic
