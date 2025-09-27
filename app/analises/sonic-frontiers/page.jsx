89df2bc3c1d71df16a2d26685a742bfa770cc842
MatheusZamo <19matheushenrique03@gmail.com>
2025-09-27 10:11:41 -0300

    feat: add image components

--- app/analises/sonic-frontiers/page.jsx
+++ app/analises/sonic-frontiers/page.jsx
@@ -1,9 +1,18 @@
 import Heading1 from "@/components/heading1"
+import Image from "next/image"
 
 const Sonic = () => {
     return(
         <>
           <Heading1>Sonic Frontiers - Análise</Heading1>
+          <Image 
+            src='/sonic-frontiers.jpg' 
+            alt='imagem do sonic-frontiers' 
+            width={640} 
+            height={360} 
+            priority
+            className='rounded-lg mt-5 mb-5'
+          />
           <p>Análise de Sonic Frontiers aqui</p>
         </>
     )
--- app/analises/super-mario-bros-wonder/page.jsx
+++ app/analises/super-mario-bros-wonder/page.jsx
@@ -1,9 +1,18 @@
 import Heading1 from "@/components/heading1"
+import Image from "next/image"
 
 const Mario = () => {
     return(
         <>
           <Heading1>Super Mario Bros. Wonder - Análise</Heading1>
+          <Image 
+            src='/super-mario-bros-wonder.jpg' 
+            alt='imagem do super-mario-bros-wonder' 
+            width={640} 
+            height={360} 
+            priority
+            className='rounded-lg mt-5 mb-5'
+          />
           <p>Análise de Super Mario Bros. Wonder aqui</p>
         </>
     )
