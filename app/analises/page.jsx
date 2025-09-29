e0bab7eb1f2bc63aff75ca362325cb2ed1ef1e13
MatheusZamo <19matheushenrique03@gmail.com>
2025-09-29 13:52:14 -0300

    feat: refactor Reviews component to use dynamic review data

--- app/analises/page.jsx
+++ app/analises/page.jsx
@@ -2,39 +2,38 @@ import Link from "next/link"
 import Heading1 from "@/components/heading1"
 import Image from "next/image"
 
+const reviews = [
+  {
+    title: 'Super Mario Bros. Wonder',
+    path: '/analises/super-mario-bros-wonder',
+    img: '/super-mario-bros-wonder.jpg'
+  }, 
+  {
+    title: 'Sonic Frontiers',
+    path: '/analises/sonic-frontiers',
+    img: '/sonic-frontiers.jpg'
+  }
+]
+
 const Reviews = () => {
     return(
         <>
           <Heading1>Análises</Heading1>
-          <ul className='flex'>
-            <li>
-              <Link href='/analises/super-mario-bros-wonder'>
-                <div className='w-[320px] h-[220px] rounded-lg bg-slate-700 m-3'>
+          <ul className='flex gap-5 mt-3'>
+            {reviews.map((review) => (
+              <li key={review.title} className='rounded-lg bg-slate-700 border border-slate-700 hover:shadow-lg'>
+                <Link href={review.path}>
                   <Image 
-                    src='/super-mario-bros-wonder.jpg'
+                    src={review.img}
                     alt='super-mario-bros-wonder'
                     width={320}
                     height={180}
                     className='rounded-lg'
                   />
-                  <p className='text-center mt-2 text-lg font-montserrat'>Super Mario Bros. Wonder</p>
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link href='/analises/sonic-frontiers'>
-                 <div className='w-[320px] h-[220px] rounded-lg bg-slate-700 m-3'>
-                  <Image 
-                    src='/sonic-frontiers.jpg'
-                    alt='sonic-frontiers'
-                    width={320}
-                    height={180}
-                    className='rounded-lg'
-                  />
-                  <p className='text-center mt-2 text-lg font-montserrat'>Sonic Frontiers</p>
-                 </div>
+                  <p className='text-center mt-2 text-lg font-montserrat'>{review.title}</p>
                 </Link>
-             </li>
+            </li>
+            ))}
           </ul>
         </>
     )
