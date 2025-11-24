57907c8088679708c4722562d830f4e0529c30a8
MatheusZamo <19matheushenrique03@gmail.com>
2025-11-24 00:24:18 -0300

    Pagination: Add searchParams & query string via Link component

--- app/analises/page.jsx
+++ app/analises/page.jsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import Heading1 from "@/components/heading1"
 import Image from "next/image"
 import { getReviews } from "../lib/get-reviews"
+import { ArrowLeft, ArrowRight } from "lucide-react"
 
 const metadata = {
   title: "Análises",
@@ -9,11 +10,23 @@ const metadata = {
 
 const dynamic = "force-dynamic"
 
-const Reviews = async () => {
+const Reviews = async ({ searchParams }) => {
   const reviews = await getReviews({ quantity: 8 })
+  const currentPage = Number(searchParams.page) || 1
+  const previousPageHref = `/analises?page=${currentPage - 1 === 0 ? 1 : currentPage - 1}`
+  const nextPageHref = `/analises?page=${currentPage + 1}`
   return (
     <>
       <Heading1>Análises</Heading1>
+      <div className="flex gap-2 py-2">
+        <Link href={previousPageHref} className="flex items-center">
+          <ArrowLeft color="#e2e8f0" size={18} />
+        </Link>
+        <span>Página {currentPage}</span>
+        <Link href={nextPageHref} className="flex items-center">
+          <ArrowRight color="#e2e8f0" size={18} />
+        </Link>
+      </div>
       <ul className="flex flex-wrap gap-5 mt-3">
         {reviews.map((review, index) => (
           <li
