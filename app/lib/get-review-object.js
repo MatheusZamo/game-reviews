e2fb6e5c9585b639b0a496565decfbc309e955a8
MatheusZamo <19matheushenrique03@gmail.com>
2025-12-22 19:19:38 -0300

    feat: Adding data fetch to the Strapi Claude server

--- app/lib/get-review-object.js
+++ app/lib/get-review-object.js
@@ -3,7 +3,10 @@ import "server-only"
 const getReviewObject = review => ({
   title: review.attributes.title,
   date: review.attributes.publishedAt.split("T")[0],
-  img: `${process.env.CMS_BASE_URL}${review.attributes.image.data.attributes.url}`,
+  img: new URL(
+    review.attributes.image.data.attributes.url,
+    process.env.CMS_BASE_URL
+  ).href,
   subtitle: review.attributes.subtitle,
 })
 
