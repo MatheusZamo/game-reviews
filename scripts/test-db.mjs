7c4efed9475b0acfe78a518638c9fe1443089f4f
MatheusZamo <19matheushenrique03@gmail.com>
2025-12-24 16:48:55 -0300

    Implementation of thequery that returns comments filtered using "where"

--- scripts/test-db.mjs
+++ scripts/test-db.mjs
@@ -4,7 +4,9 @@ const prisma = new PrismaClient()
 
 const main = async () => {
     try{
-        const comment = await prisma.comment.findMany()
+        const comment = await prisma.comment.findMany({
+            where: { slug: 'fall-guys' }
+        })
         console.log('Busca de dados realizada', comment)
     } catch (error) {
         console.error('Busca de dados com erro', error)
