9352cb3a5b763d301cdc8d8f4802213d509154aa
MatheusZamo <19matheushenrique03@gmail.com>
2025-12-24 16:24:57 -0300

    Implementing the second query that creates more than one comment in the database

--- scripts/test-db.mjs
+++ scripts/test-db.mjs
@@ -4,13 +4,31 @@ const prisma = new PrismaClient()
 
 const main = async () => {
     try{
-        const comment = await prisma.comment.create({
-            data: {
+        // const comment = await prisma.comment.create({
+        //     data: {
+        //     slug : 'fall-guys',
+        //     user: 'Joaquina',
+        //     message: 'Teste 1'
+        // }
+        // })       Cria apenas 1 novo comentario no banco 
+
+        const comment = await prisma.comment.createMany({
+            data: [
+                {
             slug : 'fall-guys',
-            user: 'Joaquina',
-            message: 'Teste 1'
-        }
+            user: 'Joaquim',
+            message: 'Teste 2'
+                }, 
+            {
+            slug : 'stardew-valley',
+            user: 'Roberto',
+            message: 'Teste 3'
+            }
+        ]
         })
+
+        //cria mais de 1 comentario no banco de dados
+
         console.log('Comentario criado', comment)
     } catch (error) {
         console.error('Erro ao criar comentario', error)
