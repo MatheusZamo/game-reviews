2a4ad064013c20fbf25890365d1f0f4a3853456c
MatheusZamo <19matheushenrique03@gmail.com>
2025-12-24 19:56:42 -0300

    Displaying the comments stored in the database on the analysis page

--- components/comment-list.jsx
+++ components/comment-list.jsx
@@ -1,21 +1,36 @@
-import { UserRound } from 'lucide-react'
+import { UserRound } from "lucide-react"
+import { PrismaClient } from "@prisma/client"
 
-const comments = [
-  { id: '1', user: 'Joaquina', message: 'Adoro esse jogo!' },
-  { id: '2', user: 'Roberto', message: 'É ok, mas não é do gênero que curto.' },
-  { id: '3', user: 'Joaquim', message: 'Não consigo parar de jogar!' }
-]
+const CommentList = async ({ slug }) => {
+  const prisma = new PrismaClient()
 
-const CommentList = () =>
-  <ul className="mt-3">
-    {comments.map(comment => (
-      <li key={comment.id} className="border-b border-slate-500 px-3 py-2 last:border-none odd:bg-slate-700">
-        <div className="flex gap-3 pb-1 text-slate-400">
-          <UserRound /> {comment.user}
-        </div>
-        <p className="italic">{comment.message}</p>
-      </li>
-    ))}
-  </ul>
+  const comments = await prisma.comment.findMany({
+    where: { slug },
+  })
+
+  return (
+    <>
+      <ul className="mt-3">
+        {comments.length > 0 &&
+          comments.map(comment => (
+            <li
+              key={comment.id}
+              className="border-b border-slate-500 px-3 py-2 last:border-none odd:bg-slate-700"
+            >
+              <div className="flex gap-3 pb-1 text-slate-400">
+                <UserRound /> {comment.user}
+              </div>
+              <p className="italic">{comment.message}</p>
+            </li>
+          ))}
+      </ul>
+      {comments.length === 0 && (
+        <p className="italic mt-3">
+          Seja o primeiro(a) a deixar um comentário!
+        </p>
+      )}
+    </>
+  )
+}
 
 export default CommentList
