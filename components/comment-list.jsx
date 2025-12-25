d0e9a7af044a1403dd8271ac7ccda945e9da4f69
MatheusZamo <19matheushenrique03@gmail.com>
2025-12-25 12:00:45 -0300

    Refactor CommentList component to simplify rendering logic for comments

--- components/comment-list.jsx
+++ components/comment-list.jsx
@@ -8,28 +8,22 @@ const CommentList = async ({ slug }) => {
     where: { slug },
   })
 
-  return (
-    <>
-      <ul className="mt-3">
-        {comments.length > 0 &&
-          comments.map(comment => (
-            <li
-              key={comment.id}
-              className="border-b border-slate-500 px-3 py-2 last:border-none odd:bg-slate-700"
-            >
-              <div className="flex gap-3 pb-1 text-slate-400">
-                <UserRound /> {comment.user}
-              </div>
-              <p className="italic">{comment.message}</p>
-            </li>
-          ))}
-      </ul>
-      {comments.length === 0 && (
-        <p className="italic mt-3">
-          Seja o primeiro(a) a deixar um comentário!
-        </p>
-      )}
-    </>
+  return comments.length > 0 ? (
+    <ul className="mt-3">
+      {comments.map(comment => (
+        <li
+          key={comment.id}
+          className="border-b border-slate-500 px-3 py-2 last:border-none odd:bg-slate-700"
+        >
+          <div className="flex gap-3 pb-1 text-slate-400">
+            <UserRound /> {comment.user}
+          </div>
+          <p className="italic">{comment.message}</p>
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <p className="italic mt-3">Seja o primeiro(a) a deixar um comentário!</p>
   )
 }
 
