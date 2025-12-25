ab03e3a6e34668c24704d19dc086d473bb0a0246
MatheusZamo <19matheushenrique03@gmail.com>
2025-12-25 15:13:53 -0300

    Create a server action to create a new comment in the database

--- components/comment-form.jsx
+++ components/comment-form.jsx
@@ -1,21 +1,54 @@
-const CommentForm = ({ title }) =>
-  <form className="border border-slate-500 flex flex-col gap-2 mt-3 px-3 py-3 rounded">
-    <p className="pb-1">Jogou <strong>{title}</strong>? Dê a sua opinião!</p>
-    <div className="flex items-center">
-      <label className="shrink-0 flex items-center space-x-2">
-        <span className="w-32">Seu nome</span>
-        <input name="userField" required className="border px-2 py-1 rounded w-48 ml-2 bg-slate-800" />
-      </label>
-    </div>
-    <div className="flex items-center">
-      <label className="shrink-0 flex items-center space-x-2">
-        <span className="w-32">Seu comentário</span>
-        <textarea name="messageField" required className="border px-2 py-1 rounded w-96 ml-2 bg-slate-800" />
-      </label>
-    </div>
-    <button type="submit" className="bg-indigo-600 rounded px-2 py-1 self-center text-slate-50 w-32 hover:bg-indigo-500">
-      Enviar
-    </button>
-  </form>
+const CommentForm = ({ slug, title }) => {
+  const createComment = async formData => {
+    "use server"
+
+    const prisma = new PrismaClient()
+    const { user, message } = Object.fromEntries(formData)
+
+    await prisma.comment.create({
+      data: {
+        slug,
+        user,
+        message,
+      },
+    })
+  }
+  return (
+    <form
+      action={createComment}
+      className="border border-slate-500 flex flex-col gap-2 mt-3 px-3 py-3 rounded"
+    >
+      <p className="pb-1">
+        Jogou <strong>{title}</strong>? Dê a sua opinião!
+      </p>
+      <div className="flex items-center">
+        <label className="shrink-0 flex items-center space-x-2">
+          <span className="w-32">Seu nome</span>
+          <input
+            name="user"
+            required
+            className="border px-2 py-1 rounded w-48 ml-2 bg-slate-800"
+          />
+        </label>
+      </div>
+      <div className="flex items-center">
+        <label className="shrink-0 flex items-center space-x-2">
+          <span className="w-32">Seu comentário</span>
+          <textarea
+            name="message"
+            required
+            className="border px-2 py-1 rounded w-96 ml-2 bg-slate-800"
+          />
+        </label>
+      </div>
+      <button
+        type="submit"
+        className="bg-indigo-600 rounded px-2 py-1 self-center text-slate-50 w-32 hover:bg-indigo-500"
+      >
+        Enviar
+      </button>
+    </form>
+  )
+}
 
 export default CommentForm
--- scripts/test-db.mjs
+++ scripts/test-db.mjs
@@ -3,16 +3,16 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 const main = async () => {
-    try{
-        const comment = await prisma.comment.findMany({
-            where: { slug: 'fall-guys' }
-        })
-        console.log('Busca de dados realizada', comment)
-    } catch (error) {
-        console.error('Busca de dados com erro', error)
-    } finally {
-        await prisma.$disconnect()
-    }
+  try {
+    const comment = await prisma.comment.findMany({
+      where: { slug: "fall-guys" },
+    })
+    console.log("Busca de dados realizada", comment)
+  } catch (error) {
+    console.error("Busca de dados com erro", error)
+  } finally {
+    await prisma.$disconnect()
+  }
 }
 
 main()
