94ff9b5fe3816e8f8a965b9bee2be2240dcd89ba
MatheusZamo <19matheushenrique03@gmail.com>
2025-12-26 11:29:40 -0300

    Implement createComment action and integrate with CommentForm component

--- components/comment-form.jsx
+++ components/comment-form.jsx
@@ -1,36 +1,35 @@
-import { prisma } from "@/app/lib/prisma"
-import { revalidatePath } from "next/cache"
+"use client"
+
+import createComment from "@/app/actions/create-comments"
+import { useState } from "react"
 
 const CommentForm = ({ slug, title }) => {
-  const createComment = async formData => {
-    "use server"
+  const [errorMessage, setErrorMessage] = useState(null)
 
-    const { user, message } = Object.fromEntries(formData)
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const result = await createComment(formData)
+    setErrorMessage(result.isError ? result.error.message : null)
 
-    await prisma.comment.create({
-      data: {
-        slug,
-        user,
-        message,
-      },
-    })
-
-    revalidatePath(`/analises/${slug}`)
+    if (!result.isError) {
+      e.target.reset()
+    }
   }
   return (
     <form
-      action={createComment}
+      onSubmit={handleSubmit}
       className="border border-slate-500 flex flex-col gap-2 mt-3 px-3 py-3 rounded"
     >
       <p className="pb-1">
         Jogou <strong>{title}</strong>? Dê a sua opinião!
       </p>
+      <input type="hidden" name="slug" value={slug} />
       <div className="flex items-center">
         <label className="shrink-0 flex items-center space-x-2">
           <span className="w-32">Seu nome</span>
           <input
             name="user"
-            required
             className="border px-2 py-1 rounded w-48 ml-2 bg-slate-800"
           />
         </label>
@@ -40,11 +39,11 @@ const CommentForm = ({ slug, title }) => {
           <span className="w-32">Seu comentário</span>
           <textarea
             name="message"
-            required
             className="border px-2 py-1 rounded w-96 ml-2 bg-slate-800"
           />
         </label>
       </div>
+      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
       <button
         type="submit"
         className="bg-indigo-600 rounded px-2 py-1 self-center text-slate-50 w-32 hover:bg-indigo-500"
